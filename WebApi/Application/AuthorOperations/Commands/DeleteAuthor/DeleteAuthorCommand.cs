b5a612a050d563a17b5cb3d2a4c588d62ee7e929
Süha Kesikbaş <suha.kesikbas@outlook.com>
2021-09-07 12:39:06 +0300

    DeleteAuthorCommand updated.

--- WebApi/Application/AuthorOperations/Commands/DeleteAuthor/DeleteAuthorCommand.cs
+++ WebApi/Application/AuthorOperations/Commands/DeleteAuthor/DeleteAuthorCommand.cs
@@ -19,6 +19,9 @@ namespace WebApi.Application.AuthorOperations.Commands.DeleteAuthor
             var author = _context.Authors.FirstOrDefault(b => b.Id == AuthorId);
             if (author is null)
                 throw new InvalidOperationException("Yazar mevcut değil!");
+            var bookOfAuthor = _context.Books.Any(b=>b.AuthorId == author.Id);
+            if (!bookOfAuthor)
+                throw new InvalidOperationException("Yazara ait yayında olan kitap var. Önce kitabı silin.");
 
             _context.Authors.Remove(author);
             _context.SaveChanges();
